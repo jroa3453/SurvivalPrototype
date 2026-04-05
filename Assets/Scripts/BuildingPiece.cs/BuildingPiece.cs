9cb0ddbf927dbf7f2b2bf86518dbad052b809334
jroa3453 <jroa3453@gmail.com>
2026-04-05 11:32:54 -0400

    BuildingUi

---
+++ Assets/Scripts/BuildingPiece.cs/BuildingPiece.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class BuildingPiece : MonoBehaviour
+{
+    public string pieceName;
+    public int logCost;
+    public int plankCost;
+    public int stoneCost;
+}
---
+++ Assets/Scripts/BuildingSystem.cs/BuildingSystem.cs
@@ -0,0 +1,158 @@
+using UnityEngine;
+
+public class BuildingSystem : MonoBehaviour
+{
+    public static BuildingSystem Instance;
+
+    public bool inBuildMode = false;
+
+    public GameObject[] buildingPieces;
+    public int selectedPieceIndex = 0;
+
+    private GameObject ghostObject;
+    public Material validMaterial;
+    public Material invalidMaterial;
+
+    private bool canPlace = false;
+    public float buildDistance = 10f;
+
+    public LayerMask buildLayerMask;
+
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+            Destroy(gameObject);
+        else
+            Instance = this;
+    }
+
+    void Update()
+    {
+        if (!inBuildMode) return;
+
+        UpdateGhostPosition();
+
+        if (Input.GetKeyDown(KeyCode.Q))
+            CyclePiece(-1);
+        if (Input.GetKeyDown(KeyCode.E) && !InventorySystem.Instance.isOpen)
+            CyclePiece(1);
+
+        if (Input.GetMouseButtonDown(0) && canPlace)
+            PlacePiece();
+
+        if (Input.GetMouseButtonDown(1))
+            ExitBuildMode();
+    }
+
+    public void EnterBuildMode()
+    {
+        inBuildMode = true;
+        SpawnGhost();
+    }
+
+    public void ExitBuildMode()
+    {
+        inBuildMode = false;
+        if (ghostObject != null)
+            Destroy(ghostObject);
+    }
+
+    void SpawnGhost()
+    {
+        if (ghostObject != null)
+            Destroy(ghostObject);
+
+        ghostObject = Instantiate(buildingPieces[selectedPieceIndex]);
+        
+        Collider col = ghostObject.GetComponent<Collider>();
+        if (col != null) col.enabled = false;
+
+        SetGhostMaterial(invalidMaterial);
+    }
+
+    void UpdateGhostPosition()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, buildDistance, buildLayerMask))
+        {
+            ghostObject.SetActive(true);
+            ghostObject.transform.position = hit.point;
+            canPlace = true;
+            SetGhostMaterial(validMaterial);
+        }
+        else
+        {
+            canPlace = false;
+            SetGhostMaterial(invalidMaterial);
+        }
+    }
+
+    Vector3 SnapToGrid(Vector3 pos)
+    {
+        float gridSize = 3f;
+        pos.x = Mathf.Round(pos.x / gridSize) * gridSize;
+        pos.z = Mathf.Round(pos.z / gridSize) * gridSize;
+        return pos;
+    }
+
+    void PlacePiece()
+    {
+        BuildingPiece piece = buildingPieces[selectedPieceIndex].GetComponent<BuildingPiece>();
+
+        if (!HasMaterials(piece))
+        {
+            Debug.Log("Not enough materials!");
+            return;
+        }
+
+        ConsumeMaterials(piece);
+        Instantiate(buildingPieces[selectedPieceIndex], ghostObject.transform.position, ghostObject.transform.rotation);
+        Debug.Log("Placed: " + piece.pieceName);
+    }
+
+    bool HasMaterials(BuildingPiece piece)
+    {
+        int logs = CountItem("Log");
+        int planks = CountItem("Plank");
+        int stones = CountItem("Stone");
+
+        Debug.Log("Logs: " + logs + " Planks: " + planks + " Stones: " + stones);
+        Debug.Log("Need - Logs: " + piece.logCost + " Planks: " + piece.plankCost + " Stones: " + piece.stoneCost);
+
+        return logs >= piece.logCost && planks >= piece.plankCost && stones >= piece.stoneCost;
+    }
+
+    void ConsumeMaterials(BuildingPiece piece)
+    {
+        if (piece.logCost > 0) InventorySystem.Instance.RemoveItem("Log", piece.logCost);
+        if (piece.plankCost > 0) InventorySystem.Instance.RemoveItem("Plank", piece.plankCost);
+        if (piece.stoneCost > 0) InventorySystem.Instance.RemoveItem("Stone", piece.stoneCost);
+    }
+
+    int CountItem(string itemName)
+    {
+        int count = 0;
+        foreach (string item in InventorySystem.Instance.itemList)
+        {
+            if (item == itemName) count++;
+        }
+        return count;
+    }
+
+    void CyclePiece(int direction)
+    {
+        selectedPieceIndex += direction;
+        if (selectedPieceIndex < 0) selectedPieceIndex = buildingPieces.Length - 1;
+        if (selectedPieceIndex >= buildingPieces.Length) selectedPieceIndex = 0;
+        SpawnGhost();
+    }
+
+    void SetGhostMaterial(Material mat)
+    {
+        Renderer[] renderers = ghostObject.GetComponentsInChildren<Renderer>();
+        foreach (Renderer r in renderers)
+            r.material = mat;
+    }
+}
