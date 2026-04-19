9135cf534c2ede612b7c0a218be20b30ff1096ac
jroa3453 <jroa3453@gmail.com>
2026-04-19 19:51:33 -0400

    Updates

---
+++ Assets/Scripts/practice script full learning.cs
@@ -0,0 +1,49 @@
+class Player
+{
+    public string Name {get; set;}
+    public int Health {get; set;}
+    public int Hunger {get; set;}
+    public bool IsAlive {get; set;}
+
+
+    public Player(string name, int health, int hunger)
+    {
+        Name = name;
+        Health = health;
+        Hunger = hunger;
+        IsAlive = true;
+    }
+
+public void PrintStatus()
+    {
+        
+
+    }
+public void TakeDamage (int amount)
+{
+    if(Player.health > 0)
+        {
+            
+            
+        }
+        
+}
+public void Eat(int amount)
+{
+        
+        
+}
+public bool CheckIsAlive()
+{
+        
+        
+}
+
+
+
+
+
+
+
+
+}
